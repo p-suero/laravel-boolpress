a5eb4e6b7118e59037df0cb95dbab9b74f84c656
Paolo <paolo.suero@gmail.com>
2020-07-13 17:26:40 +0200

    Completata autenticazione. Inizio CRUD

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.dashboard')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">Log in effettuato con successo</h1>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::prefix('/admin')->namespace('Admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('/', 'HomeController@index')->name('home');
 });
